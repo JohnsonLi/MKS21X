69a497bd6c34149f7c74b5830fbf3e15805efa2b
BuildTools <unconfigured@null.spigotmc.org>
2017-12-05 22:45:09 -0500

    added toZip

--- 07Barcode/Barcode.java
+++ 07Barcode/Barcode.java
@@ -36,14 +36,38 @@ public class Barcode /*implements Comparable<String>*/{
         return code + codeConversionCode[Integer.parseInt(getChecksum(zip))] + "|";
     }
 
-
-
-    public String toString(){
+    public static String toZip(String barcode){
+        String code = barcode.substring(1,barcode.length() - 1);
+        String zip = "";
+        String currentNum = "";
+        for (int i = 0; i < barcode.length() - 5; i++){
+            if (currentNum.length() == 5){
+                for (int ii = 0; ii < codeConversionCode.length; ii++){
+                    if(codeConversionCode[ii].equals(currentNum)){
+                        zip += Integer.toString(ii);
+                    }
+                }
+                currentNum = "";
+            }
+            try{
+                currentNum += code.substring(i, i+1);
+            }catch(StringIndexOutOfBoundsException e){
+            }
+        }
         return zip;
     }
+
+    /*public String toString(){
+        return zip;
+    }*/
 
     public static void main(String[] args) {
         Barcode code = new Barcode("11214");
         System.out.println(toCode("11214"));
+        System.out.println(toZip("|:::||:::||::|:|:::||:|::||:|::|"));
+        System.out.println(getChecksum("11214")); //9
+        System.out.println(toCode("72801"));
+        System.out.println(toZip("||:::|::|:||::|:||::::::|||::|:|"));
+        System.out.println(getChecksum("72801")); //8
     }
 }
