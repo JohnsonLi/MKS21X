1cb927c8ff05cb9c5e8c60fc3796845a06ec89c4
watermelonsoda <unconfigured@null.spigotmc.org>
2017-12-26 15:56:25 -0500

    .

--- tests/FileChooserTest.java
+++ tests/FileChooserTest.java
@@ -18,7 +18,8 @@ public class FileChooserTest extends JFrame implements ActionListener{
 
     public FileChooserTest(){
         setTitle("FileChooserTest");
-        setLocationRelativeTo(null);
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
         container = this.getContentPane();
